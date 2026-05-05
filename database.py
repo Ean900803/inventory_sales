5c807d4c433ab8def1afcdbc03eb7c803812b489
ean <jr00631517@gmail.com>
2026-05-06 00:28:36 +0800

    feat: 1.新增簡易版本的schema 2.qtdesinger拉好Mainwindow 3.database

--- database.py
+++ database.py
@@ -4,13 +4,19 @@ from contextlib import contextmanager
 from config import DB_CONFIG
 
 @contextmanager
-def get_connection():
+def get_connection(transaction=False):
+    # 加上DictCursot 確保取値時可以使用col name做為key取value，而非tuple
     conn = pymysql.connect(**DB_CONFIG, cursorclass=DictCursor)
     try:
-        yield conn
+        yield conn # 每次透過with取得連線併使用這邊的try catch
+        if transaction:
+            conn.commit()
+    except Exception as e:
+        if transaction:
+            conn.rollback()
+        raise e
     finally:
         conn.close()
-
 def test_connection():
     """啟動時測試DB連線"""
     try:
--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QMessageBox
 from database import test_connection
+from views.main_window_ui import Ui_MainWindow
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
         self.setWindowTitle("訂單管理系統")
-        self.resize(1200, 800)
-        self.setCentralWidget(QLabel("Hello OMS", self))
+        self.page_map = {
+            "員工管理": self.ui.emp,
+            "商品管理": self.ui.product,
+            "類別管理": self.ui.category,
+            "客戶管理": self.ui.customer,
+            "訂單管理": self.ui.order
+        }
+
+        self.ui.sideBar.currentTextChanged.connect(self.switch_page)
+    def switch_page(self, text):
+        page = self.page_map.get(text)
+        if page:
+            self.ui.content_stack.setCurrentWidget(page)
 
 def main():
+    app = QApplication(sys.argv)
     if not test_connection():
-        print("資料庫連線失敗，請檢查 .env 設定")
+        QMessageBox.critical(None, "連線失敗", "資料庫連線失敗，請檢查.env設定")
         sys.exit(1)
     
-    app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
